9509e654d449641df0480352a5f2294bbaa2bd7c
0x0c <akira.m.itachi@gmail.com>
2011-12-14 04:30:33 +0900

    first commit
    first commit

---
+++ CIPViewController.h
@@ -0,0 +1,18 @@
+//
+//  CIPViewController.h
+//  CustomImagePickerTest
+//
+//  Created by 暁 松田 on 11/11/19.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CIPViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+{
+	
+}
+
+- (IBAction)showPicker:(id)sender;
+
+@end
